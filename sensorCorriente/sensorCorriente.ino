18a826d38613c1e2a8ea2bdd4e1e8acd537c7cbd
juank0814 <juank0814@hotmail.com>
2017-09-08 14:32:18 -0500

    declararo las funciones
    tengo las funciones de corte y coagulacion

--- sensorCorriente/sensorCorriente.ino
+++ sensorCorriente/sensorCorriente.ino
@@ -4,17 +4,33 @@
 //La cantidad de muestras para calcular el promedio depende del nivel de ruido que tengan.
 
 float Sensibilidad=0.100;       //sensibilidad en voltios dependiendo el sensor 20A
+float Vsensor=0;
 int ApagoCorte=8;
 int ApagoCoag=7;
+int estadoBotonCor=0;
+int estadoBotonCoag=0;
+int Cor_on=2;
+int Coag_on=3;
 
 void setup() {
   Serial.begin(9600);
   pinMode(ApagoCorte,OUTPUT);
   pinMode(ApagoCoag,OUTPUT);
+  pinMode(Cor_on,INPUT); //Establecemos el pin 2 BotonCorte como entrada
+  pinMode(Coag_on,INPUT); //Establecemos el pin 3 BotonCoag como entrada
 }
 
 void loop() {
   float Idc=calculoCorriente(500);    //obtenemos la corriente promedio de 500 muestras 
+ 
+  estadoBotonCor=digitalRead(Cor_on);  //almacena la lectura digital 
+  if(estadoBotonCor == HIGH){
+  Vsensor=analogRead(A0);
+  }
+   estadoBotonCoag=digitalRead(Coag_on);
+  if(estadoBotonCoag == HIGH){
+   Vsensor=analogRead(A1);
+  }
   Serial.print("CorrienteCor: ");
   Serial.println(Idc,3);              //imprimir la corriente con 3 decimales
   delay(100);
@@ -24,10 +40,10 @@ float calculoCorriente(int numeroMuestras)
   float VoltajeSensorCOR = 0;
   float VoltajeSensorCOAG = 0;
   float intensidad = 0;
- 
+   
   for(int i=0;i<numeroMuestras;i++)                               //ciclo donde sumamos las 500 muestras
   {
-    VoltajeSensorCOR = analogRead(A0) * (5.0/1023.0);              //lee el canal A0 y calcula el valor en tension 
+    VoltajeSensorCOR = Vsensor * (5.0/1023.0);              //lee el canal A0 y calcula el valor en tension 
     intensidad=intensidad+(VoltajeSensorCOR-2.5087)/Sensibilidad;    //calculamos corriente y sumamos
   }
   intensidad=intensidad/numeroMuestras;
