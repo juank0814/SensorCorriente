c3fbcd9974def637999302d9cac06c3034144e10
juank0814 <juank0814@hotmail.com>
2017-09-08 11:18:21 -0500

    calibracion del sensor
    se tuvo encuenta el offset

--- sensorCorriente/sensorCorriente.ino
+++ sensorCorriente/sensorCorriente.ino
@@ -1,34 +1,42 @@
+//Aplicando Filtro a las lecturas del ACS712
+//usaremos la media aritmética de varias lecturas consecutivas, 
+//simplemente hay que sumar la lecturas y dividirlas en el número de muestras.
+//La cantidad de muestras para calcular el promedio depende del nivel de ruido que tengan.
+
 float Sensibilidad=0.100;       //sensibilidad en voltios dependiendo el sensor 20A
-int apagocorte=8;
+int ApagoCorte=8;
+int ApagoCoag=7;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(apagocorte,OUTPUT);
+  pinMode(ApagoCorte,OUTPUT);
+  pinMode(ApagoCoag,OUTPUT);
 }
 
 void loop() {
   float Idc=calculoCorriente(500);    //obtenemos la corriente promedio de 500 muestras 
-  Serial.print("Corriente: ");
+  Serial.print("CorrienteCor: ");
   Serial.println(Idc,3);              //imprimir la corriente con 3 decimales
   delay(100);
 }
 float calculoCorriente(int numeroMuestras)
 {
-  float leerAcs712 = 0;
+  float VoltajeSensorCOR = 0;
+  float VoltajeSensorCOAG = 0;
   float intensidad = 0;
-  for(int i=0;i<numeroMuestras;i++)
+ 
+  for(int i=0;i<numeroMuestras;i++)                               //ciclo donde sumamos las 500 muestras
   {
-    leerAcs712 = analogRead(A0) * (5.0/1023.0);              //lee el canal A0 y calcula el valor en tension 
-    intensidad=intensidad+(leerAcs712-2.5)/Sensibilidad;    //calculamos corriente y sumamos
+    VoltajeSensorCOR = analogRead(A0) * (5.0/1023.0);              //lee el canal A0 y calcula el valor en tension 
+    intensidad=intensidad+(VoltajeSensorCOR-2.5087)/Sensibilidad;    //calculamos corriente y sumamos
   }
   intensidad=intensidad/numeroMuestras;
   if( intensidad >= 0.5){
-    digitalWrite (apagocorte,HIGH);
+    digitalWrite (ApagoCorte,HIGH);
   }
   else{
-   digitalWrite (apagocorte,LOW); 
+   digitalWrite (ApagoCorte,LOW); 
   }
   return(intensidad);
-  
-  
 }
 
